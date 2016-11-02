2108f40e9c501ee4cee7db00fa6dcf5a2571d594
Stephen Weston <stephen.weston@yale.edu>
2016-11-02 08:20:02 -0400

    Changed label names in microbenchmark.

--- preallocate.R
+++ preallocate.R
@@ -4,6 +4,7 @@
 # Note that tracemem events are not triggered in this case
 # even though the result vector is copied.
 
+# Classic bad for loop
 bad <- function(n, x) {
   result <- NULL
   for (i in 1:n) {
@@ -12,6 +13,7 @@ bad <- function(n, x) {
   result
 }
 
+# Demonstrates strange way to improve performance
 strange <- function(n, x) {
   result <- NULL
   for (i in n:1) {
@@ -20,6 +22,7 @@ strange <- function(n, x) {
   result
 }
 
+# Standard preallocation of result vector
 okay <- function(n, x) {
   result <- double(n)
   for (i in 1:n) {
@@ -28,16 +31,19 @@ okay <- function(n, x) {
   result
 }
 
+# use of vector assignment
 better <- function(n, x) {
   result <- double(n)
   result[] <- x
   result
 }
 
+# use of standard R function
 best <- function(n, x) {
   rep(x, n)
 }
 
+# Generate test data
 n <- 1000
 
 # Verify correct results
@@ -54,10 +60,10 @@ identical(a1, a4)
 # Run benchmarks
 library(microbenchmark)
 res <- microbenchmark(
-  a1=bad(n, 7),
-  a1.2=strange(n, 7),
-  a2=okay(n, 7),
-  a3=better(n, 7),
-  a4=best(n, 7)
+  bad=bad(n, 7),
+  strange=strange(n, 7),
+  okay=okay(n, 7),
+  better=better(n, 7),
+  best=best(n, 7)
 )
 print(res)
