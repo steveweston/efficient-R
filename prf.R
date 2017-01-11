dfd993746bbcaa314adf085149c01b325a5e4368
Stephen Weston <stephen.weston@yale.edu>
2017-01-10 22:28:28 -0500

    Increased size of problem and fixed bad value of ntree.

--- prf.R
+++ prf.R
@@ -9,15 +9,15 @@ library(randomForest)
 worker <- function(n) randomForest(x, y, ntree=n)
 
 # Generate test data
-x <- matrix(runif(500), 100)
-y <- gl(2, 50)
+x <- matrix(runif(5000), 1000)
+y <- gl(2, 500)
 ntree <- 1000
 cores <- detectCores()
 vntree <- rep(ntree %/% cores, cores)
 
 # Run benchmarks
 res <- microbenchmark(
-  rf=randomForest(x, y, ntree=10000),
+  rf=randomForest(x, y, ntree=ntree),
   prf=do.call('combine', mclapply(vntree, worker, mc.cores=cores))
 )
 print(res)
